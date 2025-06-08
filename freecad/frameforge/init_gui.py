024f5133ae06c7ebf75353b5ccdc2a3a393abdfe
Ian Abreu <z0r0@shurikenlabs.com>
2025-06-08 19:06:35 -0400

    reverting some lints

--- freecad/frameforge/init_gui.py
+++ freecad/frameforge/init_gui.py
@@ -1,34 +1,38 @@
 import os
-
-import FreeCAD as App
 import FreeCADGui as Gui
-from freecad.frameforge import ICONPATH, TRANSLATIONSPATH
+import FreeCAD as App
 from freecad.frameforge.translate_utils import translate
+
+from freecad.frameforge import ICONPATH, TRANSLATIONSPATH
 
 
 class FrameForge(Gui.Workbench):
     """
     class which gets initiated at startup of the gui
     """
-
     MenuText = translate("frameforge", "FrameForge")
     ToolTip = translate("frameforge", "a simple FrameForge")
     Icon = os.path.join(ICONPATH, "metalwb.svg")
 
-    toolbox_drawing = ["Sketcher_NewSketch", "FrameForge_ParametricLine"]
+    toolbox_drawing = [
+        "Sketcher_NewSketch",
+        "FrameForge_ParametricLine"
+    ]
 
     toolbox_frameforge = [
         "FrameForge_CreateProfiles",
         "FrameForge_TrimProfiles",
-        "FrameForge_EndMiter",
+        "FrameForge_EndMiter"
     ]
 
     toolbox_part = [
         "Part_Fuse",
         "Part_Cut",
+
         "PartDesign_Body",
+
         "PartDesign_Pad",
-        "PartDesign_Pocket",
+        "PartDesign_Pocket"
     ]
 
     def GetClassName(self):
@@ -39,51 +43,41 @@ class FrameForge(Gui.Workbench):
         This function is called at the first activation of the workbench.
         here is the place to import all the commands
         """
-        from freecad.frameforge import (create_end_miter_tool,
-                                        create_profiles_tool,
-                                        create_trimmed_profiles_tool,
-                                        parametric_line)
+        from freecad.frameforge import parametric_line
+        from freecad.frameforge import create_profiles_tool, create_trimmed_profiles_tool, create_end_miter_tool
 
         # Add translations path
         Gui.addLanguagePath(TRANSLATIONSPATH)
         Gui.updateLocale()
 
-        App.Console.PrintMessage(
-            translate("frameforge", "Switching to frameforge") + "\n"
-        )
+        App.Console.PrintMessage(translate(
+            "frameforge",
+            "Switching to frameforge") + "\n")
 
-        self.appendToolbar(
-            translate("frameforge", "Drawing Primitives"), self.toolbox_drawing
-        )
-        self.appendMenu(
-            translate("frameforge", "Drawing Primitives"), self.toolbox_drawing
-        )
+        self.appendToolbar(translate("frameforge", "Drawing Primitives"), self.toolbox_drawing)
+        self.appendMenu(translate("frameforge", "Drawing Primitives"), self.toolbox_drawing)
 
-        self.appendToolbar(
-            translate("frameforge", "Frameforge"), self.toolbox_frameforge
-        )
+        self.appendToolbar(translate("frameforge", "Frameforge"), self.toolbox_frameforge)
         self.appendMenu(translate("frameforge", "Frameforge"), self.toolbox_frameforge)
 
-        self.appendToolbar(
-            translate("frameforge", "Part Primitives"), self.toolbox_part
-        )
+        self.appendToolbar(translate("frameforge", "Part Primitives"), self.toolbox_part)
         self.appendMenu(translate("frameforge", "Part Primitives"), self.toolbox_part)
 
     def Activated(self):
-        """
+        '''
         code which should be computed when a user switch to this workbench
-        """
-        App.Console.PrintMessage(
-            translate("frameforge", "Workbench frameforge activated.") + "\n"
-        )
+        '''
+        App.Console.PrintMessage(translate(
+            "frameforge",
+            "Workbench frameforge activated.") + "\n")
 
     def Deactivated(self):
-        """
+        '''
         code which should be computed when this workbench is deactivated
-        """
-        App.Console.PrintMessage(
-            translate("frameforge", "Workbench frameforge de-activated.") + "\n"
-        )
+        '''
+        App.Console.PrintMessage(translate(
+            "frameforge",
+            "Workbench frameforge de-activated.") + "\n")
 
 
 Gui.addWorkbench(FrameForge())
--- freecad/frameforge/parametric_line.py
+++ freecad/frameforge/parametric_line.py
@@ -5,29 +5,24 @@
 # Select 2 vertexes in the 3D View and activate the tool.
 
 import os
-
 import FreeCAD as App
 import Part
 
 if App.GuiUp:
     import FreeCADGui as Gui
 
-from freecad.frameforge import ICONPATH, _utils
+from freecad.frameforge import _utils
+from freecad.frameforge import ICONPATH
 
 TOOL_ICON = os.path.join(ICONPATH, "line.svg")
 
 
 class ParametricLine:
     """Creates a parametric line between two vertexes"""
-
     def __init__(self, obj):
         """Add the properties"""
-        obj.addProperty(
-            "App::PropertyLinkSub", "Vertex1", "ParametricLine", "First Vertex"
-        )
-        obj.addProperty(
-            "App::PropertyLinkSub", "Vertex2", "ParametricLine", "Second Vertex"
-        )
+        obj.addProperty("App::PropertyLinkSub", "Vertex1", "ParametricLine", "First Vertex")
+        obj.addProperty("App::PropertyLinkSub", "Vertex2", "ParametricLine", "Second Vertex")
         obj.Proxy = self
 
     def execute(self, obj):
@@ -60,11 +55,8 @@ class ParametricLineViewProvider:
 
 class CreateParametricLineCommand:
     """Creates a parametric line between two vertexes"""
-
     def make_parametric_line(self, source):
-        line_object = App.ActiveDocument.addObject(
-            "Part::FeaturePython", "ParametricLine"
-        )
+        line_object = App.ActiveDocument.addObject("Part::FeaturePython", "ParametricLine")
 
         ParametricLine(line_object)
         ParametricLineViewProvider(line_object.ViewObject)
@@ -98,11 +90,10 @@ class CreateParametricLineCommand:
             return False
 
     def GetResources(self):
-        return {
-            "Pixmap": TOOL_ICON,
-            "MenuText": "Create a Parametric Line",
-            "ToolTip": "Create a Parametric Line from two Vertex<br><br>Select two vertex then run this command",
-        }
+        return {'Pixmap': TOOL_ICON,
+                'MenuText': "Create a Parametric Line",
+                'ToolTip': "Create a Parametric Line from two Vertex<br><br>Select two vertex then run this command"
+            }
 
 
-Gui.addCommand("FrameForge_ParametricLine", CreateParametricLineCommand())
+Gui.addCommand('FrameForge_ParametricLine', CreateParametricLineCommand())
