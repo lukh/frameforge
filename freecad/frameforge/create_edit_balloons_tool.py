d3c4132dfeaa96d61f7f7cd62a4f028493ea94a0
Vivien Henry <vivien.henry@inductivebrain.fr>
2026-02-09 15:33:48 +0100

    add populate id and create balloons :)

---
+++ freecad/frameforge/create_edit_balloons_tool.py
@@ -0,0 +1,199 @@
+import os
+from collections import defaultdict
+
+import FreeCAD as App
+import FreeCADGui as Gui
+
+from freecad.frameforge._utils import (
+    is_extrudedcutout,
+    is_fusion,
+    is_group,
+    is_part,
+    is_profile,
+    is_trimmedbody, 
+    is_link,
+    get_profiles_and_links_from_object
+)
+
+
+from freecad.frameforge.ff_tools import ICONPATH, PROFILEIMAGES_PATH, PROFILESPATH, UIPATH, translate
+
+
+
+
+def place_balloon(balloon):
+    view = balloon.SourceView
+    P = balloon.Target.Shape.CenterOfGravity
+
+    D = view.Direction.normalize()
+    X = view.XDirection.normalize()
+    Y = D.cross(X).normalize()
+
+    C = view.getGeometricCenter()
+
+    d = P.sub(C)
+    x2d = d.dot(X) * view.Scale
+    y2d = d.dot(Y) * view.Scale
+    
+    pageX = view.X.Value + x2d
+    pageY = view.Y.Value + y2d
+
+    balloon.OriginX = x2d
+    balloon.OriginY = y2d
+
+    balloon.X = x2d + 20
+    balloon.Y = y2d + 20
+	
+def create_balloon(view, src_obj):
+    doc = App.ActiveDocument
+    page = None
+    for obj in doc.Objects:
+        if obj.TypeId == "TechDraw::DrawPage":
+            if hasattr(obj, "Views"):
+                for v in obj.Views:
+                    if view == v:
+                        page = obj
+                        break
+                    elif v.TypeId == "TechDraw::DrawProjGroup":
+                        for vv in v.Views:
+                            if view == vv:
+                                page = obj
+                                break
+    if page is None:
+        raise RuntimeError("Impossible de trouver la page contenant la vue")
+
+    balloon = doc.addObject('TechDraw::DrawViewBalloon', f"Balloon_{src_obj.Label}")
+    balloon.addProperty("App::PropertyLink", "Target", "FrameForge", "Target Profile").Target = src_obj
+    balloon.SourceView = view
+    balloon.Text = src_obj.PID
+    page.addView(balloon)
+
+    place_balloon(balloon)
+
+
+
+
+class CreateBalloonsCommand:
+    def GetResources(self):
+        return {
+            "Pixmap": os.path.join(ICONPATH, "create-balloons.svg"),
+            "MenuText": translate("FrameForge", "Create Balloons"),
+            "Accel": "M, B",
+            "ToolTip": translate(
+                "FrameForge",
+                "<html><head/><body><p><b>Automatically Create balloons for selected Profiles/Links</b> \
+                    <br><br> \
+                    select profiles, links \
+                    </p></body></html>",
+            ),
+        }
+
+    def IsActive(self):
+        if App.ActiveDocument:
+            selection = Gui.Selection.getSelection()
+
+            views = [s for s in selection if s.TypeId == "TechDraw::DrawProjGroupItem"]
+            if len(views) != 1:
+                return False
+
+            objects = [
+                    sel
+                    for sel in selection if 
+                    is_fusion(sel)
+                    or is_part(sel)
+                    or is_group(sel)
+                    or is_profile(sel)
+                    or is_trimmedbody(sel)
+                    or is_extrudedcutout(sel)
+                    or is_link(sel)
+                ]
+
+            if len(objects) <= 0:
+                return False
+
+            return True
+
+        return False
+
+    def Activated(self):
+        selection = Gui.Selection.getSelection()
+
+        views = [s for s in selection if s.TypeId == "TechDraw::DrawProjGroupItem"]
+        objects = [
+                sel
+                for sel in selection if 
+                is_fusion(sel)
+                or is_part(sel)
+                or is_group(sel)
+                or is_profile(sel)
+                or is_trimmedbody(sel)
+                or is_extrudedcutout(sel)
+                or is_link(sel)
+            ]
+
+        if len(objects) <= 0 or len(views) != 1:
+            return
+
+        view = views[0]
+
+
+        App.ActiveDocument.openTransaction("Create Balloons")
+
+        sel_profiles, sel_links = [], []
+        for s in objects:
+            get_profiles_and_links_from_object(sel_profiles, sel_links, s)
+        
+
+        ff_objects = sel_profiles + sel_links
+
+        for o in ff_objects:
+            create_balloon(view, o)
+
+
+        App.ActiveDocument.commitTransaction()
+        App.ActiveDocument.recompute()
+
+
+
+
+class ResfreshBalloonsCommand:
+    def GetResources(self):
+        return {
+            "Pixmap": os.path.join(ICONPATH, "refresh-balloons.svg"),
+            "MenuText": translate("FrameForge", "Resfresh Balloons"),
+            "Accel": "M, B",
+            "ToolTip": translate(
+                "FrameForge",
+                "<html><head/><body><p><b>Refresh selected balloons</b> \
+                    </p></body></html>",
+            ),
+        }
+
+    def IsActive(self):
+        if App.ActiveDocument:
+            selection = Gui.Selection.getSelection()
+
+            balloons = [b for b in selection if b.TypeId == "TechDraw::DrawViewBalloon" and hasattr(b, "Target")]
+            if len(balloons) <= 0:
+                return False
+            return True
+
+        return False
+
+    def Activated(self):
+        selection = Gui.Selection.getSelection()
+
+        App.ActiveDocument.openTransaction("Resfresh Balloons")
+
+        balloons = [b for b in selection if b.TypeId == "TechDraw::DrawViewBalloon" and hasattr(b, "Target")]
+        for b in balloons:
+            place_balloon(b)
+
+
+        App.ActiveDocument.commitTransaction()
+        App.ActiveDocument.recompute()
+
+
+
+Gui.addCommand("FrameForge_CreateBalloons", CreateBalloonsCommand())
+Gui.addCommand("FrameForge_RefreshBalloons", ResfreshBalloonsCommand())
---
+++ freecad/frameforge/populate_ids_tool.py
@@ -0,0 +1,257 @@
+
+
+#Expected behaviour:
+
+# POPULATE
+# Number/Letter for Profiles/Links
+# - All numbers
+# - All letters
+# - Number for Profiles, Letters for Link
+# - Number for Link, Letters for Profiles
+
+# Allow duplicated numbering
+# Keep existing
+
+# Start at specific number/letter
+# Fill when available (document)
+# Fill when available (selection)
+# Start at last of document
+
+
+# Algorithm:
+
+# - get all documents objects (flat)
+# - get all selected objects (flat)
+
+
+# RESET
+# Reset All / Reset Selected if objects are selected
+
+
+
+
+import os
+from collections import defaultdict
+
+import FreeCAD as App
+import FreeCADGui as Gui
+
+from freecad.frameforge._utils import (
+    is_extrudedcutout,
+    is_fusion,
+    is_group,
+    is_part,
+    is_profile,
+    is_trimmedbody, 
+    is_link,
+    get_profiles_and_links_from_object, 
+    get_profiles_and_links_from_document
+)
+
+
+from freecad.frameforge.ff_tools import ICONPATH, PROFILEIMAGES_PATH, PROFILESPATH, UIPATH, translate
+from freecad.frameforge.populate_ids import populate_ids
+
+
+
+
+
+
+
+class PopulateIDsTaskPanel:
+    def __init__(self):
+        self.form = Gui.PySideUic.loadUi(os.path.join(UIPATH, "populate_ids.ui"))
+
+        param = App.ParamGet("User parameter:BaseApp/Preferences/Frameforge")
+        if not param.IsEmpty():
+            self.form.cb_allow_duplicated.setChecked(param.GetBool("Allow Duplicating IDs", False))
+            self.form.cb_reset_numbering.setChecked(param.GetBool("Reset Numbering IDs", False))
+            self.form.cb_numbering_type.setCurrentIndex(param.GetInt("IDs numbering type", 0))
+            self.form.cb_numbering_scheme.setCurrentIndex(param.GetInt("IDs numbering scheme", 0))
+            self.form.sp_first_number.setValue(param.GetInt("First Number ID", 0))
+            self.form.le_first_letter.setText(param.GetString("First Letter ID", "A"))
+
+            
+
+    def open(self):
+        App.Console.PrintMessage(translate("frameforge", "Opening Populate IDs\n"))
+
+        # create a TrimmedProfile object
+        App.ActiveDocument.openTransaction("Populate IDs")
+
+    def reject(self):
+        App.Console.PrintMessage(translate("frameforge", "Rejecting Populate IDs\n"))
+
+        self.clean()
+        App.ActiveDocument.abortTransaction()
+
+        return True
+
+    def accept(self):
+        sel = Gui.Selection.getSelection()
+
+        if all(
+                [
+                    is_fusion(sel)
+                    or is_part(sel)
+                    or is_group(sel)
+                    or is_profile(sel)
+                    or is_trimmedbody(sel)
+                    or is_extrudedcutout(sel)
+                    or is_link(sel)
+                    for sel in Gui.Selection.getSelection()
+                ]
+            ):
+            param = App.ParamGet("User parameter:BaseApp/Preferences/Frameforge")
+            param.SetBool("Allow Duplicating IDs", self.form.cb_allow_duplicated.isChecked())
+            param.SetBool("Reset Numbering IDs", self.form.cb_reset_numbering.isChecked())
+            param.SetInt("IDs numbering type", self.form.cb_numbering_type.currentIndex())
+            param.SetInt("IDs numbering scheme", self.form.cb_numbering_scheme.currentIndex())
+
+            param.SetInt("First Number ID", self.form.sp_first_number.value())
+            param.SetString("First Letter ID", self.form.le_first_letter.text())
+
+            sel_profiles, sel_links = [], []
+            for s in sel:
+                get_profiles_and_links_from_object(sel_profiles, sel_links, s)
+
+            doc_profiles, doc_links = get_profiles_and_links_from_document()
+
+            numbering_type = ["all_numbers", "all_letters", "number_for_profiles_letters_for_links" ,"letters_for_profiles_number_for_links"][self.form.cb_numbering_type.currentIndex()]
+            allow_duplicated = self.form.cb_allow_duplicated.isChecked()
+            reset_existing = self.form.cb_reset_numbering.isChecked()
+            numbering_scheme = ["fill_selection", "fill_document", "continue_document", "start_at"][self.form.cb_numbering_scheme.currentIndex()]
+
+
+            populate_ids(sel_profiles, sel_links, doc_profiles, doc_links, 
+                numbering_type,
+                allow_duplicated,
+                reset_existing,
+                numbering_scheme,
+                start_number=str(self.form.sp_first_number.value()), start_letter=self.form.le_first_letter.text())
+
+
+            App.ActiveDocument.commitTransaction()
+            App.ActiveDocument.recompute()
+
+            return True
+
+        else:
+            App.ActiveDocument.abortTransaction()
+            return False
+
+    def clean(self):
+        pass
+
+
+
+
+
+
+
+class PopulateIDsCommand:
+    def GetResources(self):
+        return {
+            "Pixmap": os.path.join(ICONPATH, "populate-ids.svg"),
+            "MenuText": translate("FrameForge", "Populate IDs"),
+            "Accel": "M, B",
+            "ToolTip": translate(
+                "FrameForge",
+                "<html><head/><body><p><b>Populate IDs of selected Frameforge objects</b> \
+                    <br><br> \
+                    select profiles, links or groups, parts \
+                    </p></body></html>",
+            ),
+        }
+
+    def IsActive(self):
+        if App.ActiveDocument:
+            if len(Gui.Selection.getSelection()) >= 1:
+                return all(
+                    [
+                        is_fusion(sel)
+                        or is_part(sel)
+                        or is_group(sel)
+                        or is_profile(sel)
+                        or is_trimmedbody(sel)
+                        or is_extrudedcutout(sel)
+                        or is_link(sel)
+                        for sel in Gui.Selection.getSelection()
+                    ]
+                )
+
+        return False
+
+    def Activated(self):
+        panel = PopulateIDsTaskPanel()
+        Gui.Control.showDialog(panel)
+
+
+
+
+class ResetIDsCommand:
+    def GetResources(self):
+        return {
+            "Pixmap": os.path.join(ICONPATH, "reset-ids.svg"),
+            "MenuText": translate("FrameForge", "Reset IDs"),
+            "Accel": "M, B",
+            "ToolTip": translate(
+                "FrameForge",
+                "<html><head/><body><p><b>Reset IDs of selected Frameforge objects</b> \
+                    <br><br> \
+                    select profiles, links or groups, parts \
+                    </p></body></html>",
+            ),
+        }
+
+    def IsActive(self):
+        if App.ActiveDocument:
+            if len(Gui.Selection.getSelection()) >= 1:
+                return all(
+                    [
+                        is_fusion(sel)
+                        or is_part(sel)
+                        or is_group(sel)
+                        or is_profile(sel)
+                        or is_trimmedbody(sel)
+                        or is_extrudedcutout(sel)
+                        or is_link(sel)
+                        for sel in Gui.Selection.getSelection()
+                    ]
+                )
+
+        return False
+
+    def Activated(self):
+        sel = Gui.Selection.getSelection()
+
+        if all(
+                [
+                    is_fusion(sel)
+                    or is_part(sel)
+                    or is_group(sel)
+                    or is_profile(sel)
+                    or is_trimmedbody(sel)
+                    or is_extrudedcutout(sel)
+                    or is_link(sel)
+                    for sel in Gui.Selection.getSelection()
+                ]
+            ):
+
+
+            App.ActiveDocument.openTransaction("Reset IDs")
+            sel_profiles, sel_links = [], []
+            for s in sel:
+                get_profiles_and_links_from_object(sel_profiles, sel_links, s)
+
+            for o in sel_profiles+sel_links:
+                o.PID = ""
+
+            App.ActiveDocument.commitTransaction()
+            App.ActiveDocument.recompute()
+            
+
+Gui.addCommand("FrameForge_PopulateIDs", PopulateIDsCommand())
+Gui.addCommand("FrameForge_ResetIDs", ResetIDsCommand())
+
+
