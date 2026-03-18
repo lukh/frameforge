b23fde97c4b3a709844d0dccec2367b6278b8a9c
Roman Yakovskiy <zerr0@inbox.ru>
2026-03-18 09:33:42 +0500

    CenterOfGravity is not the best choice for asymetric profiles

--- freecad/frameforge/trimmed_profile.py
+++ freecad/frameforge/trimmed_profile.py
@@ -196,8 +196,8 @@ class TrimmedProfile:
 
                 normal = Part.Plane(p1, p2, p3).toShape().normalAt(0, 0)
                 intersection = shape1.common(shape2)
-                cutplane = Part.makePlane(10, 10, intersection.CenterOfGravity, vec1, normal)
-                cutplane.rotate(intersection.CenterOfGravity, normal, -90 + bisect)
+                cutplane = Part.makePlane(10, 10, intersection.BoundBox.Center, vec1, normal)
+                cutplane.rotate(intersection.BoundBox.Center, normal, -90 + bisect)
                 cut_shapes.append(self.getOutsideCV(cutplane, fp.TrimmedBody.Shape))
 
         if len(cut_shapes) > 0:
